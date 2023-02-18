b44f2c94614c99ab6001c9858fc9a67414307fc7
levifeyd <kuleshov-as@mail.ru>
2023-02-18 14:23:29 +0400

    refactor acount view

--- legalConsultation/resources/views/partials/header.blade.php
+++ legalConsultation/resources/views/partials/header.blade.php
@@ -1,24 +1,18 @@
 <nav class="">
-    <div class="mb-2 sm:mb-0 inner">
-{{--        <span class="text-xs text-grey-dark">Юридическая консультация</span>--}}
+    <div class="mb-2 sm:mb-0 inner" style="text-align: center">
         <h1 class="text-xl text-gray-800">Юридическая консутльтация</h1>
         <h5 class="text-xl text-gray-800">Здравствуйте
             @if(isset($user))
                 {{ $user->name }}
             @endif
         !</h5>
-    </div>
-{{--    <div class="sm:mb-0 self-right">--}}
+{{--    </div>--}}
         @auth("web")
-            <div style="float: right">
-                <a href="{{route('logout')}}" class="tab">Выйти</a>
-            </div>
-                <a href="{{ route('account') }}" class="text-indigo-600 hover:text-indigo-900">Мой кабинет</a>
+            <a href="{{ route('account') }}" class="btn btn-primary">Мой кабинет</a>
+            <a href="{{route('logout')}}" class="btn btn-primary">Выйти</a>
         @endauth
         @guest("web")
-        <div style="float: right">
             <a href="{{route('login')}}" class="text-md">Войти</a>
-        </div>
         @endguest
-{{--    </div>--}}
+    </div>
 </nav>
