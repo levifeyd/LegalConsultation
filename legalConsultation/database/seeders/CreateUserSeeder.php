bdb6a6d41033500401e45d573d7ccdbff7ce4572
levifeyd <kuleshov-as@mail.ru>
2023-02-23 13:11:32 +0400

    pass on seed

--- legalConsultation/database/seeders/CreateUserSeeder.php
+++ legalConsultation/database/seeders/CreateUserSeeder.php
@@ -23,6 +23,21 @@ class CreateUserSeeder extends Seeder
             'email' => 'Polina@mail.ru',
             'password' => bcrypt('qweasdzxc'),
         ]);
+//        $user = User::query()->create([
+//            'name' => 'LawyerJack',
+//            'email' => 'LawyerJack@mail.ru',
+//            'password' => bcrypt('qweasdzxc'),
+//        ]);
+        //     $user = User::query()->create([
+//            'name' => 'LawyerAlex',
+//            'email' => 'LawyerAlex@mail.ru',
+//            'password' => bcrypt('qweasdzxc'),
+//        ]);
+//            $user = User::query()->create([
+//            'name' => 'Artem',
+//            'email' => 'artem@mail.ru',
+//            'password' => bcrypt('qweasdzxc'),
+//        ]);
 
         Role::create([
             'name'=>'user',
